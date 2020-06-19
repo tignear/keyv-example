7a3e24990cb55ccc9e7c4217a23a7dc35951b827
tignear <tignear+m@gmail.com>
2020-06-19 23:08:57 +0900

    fix

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ const keyv = new Keyv('postgresql://postgres:arfw5369@localhost:5432/keyvtest',{
 keyv.on("error",e=>console.log("error:",e))
 async function main(){
     console.log("start");
-    console.log(await (await keyv.opts.store.opts.connect())("version()"))
     await keyv.set("cat","nyan").catch(e=>console.log("error:",e));
     console.log("set");
     console.log(await keyv.get("cat"));
