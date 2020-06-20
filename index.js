3377338c79392b8e3d2f9d8ea843cfe939fc2bb3
tignear <tignear+m@gmail.com>
2020-06-20 14:31:26 +0900

    update

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const Keyv =require("keyv");
-const keyv = new Keyv("postgresql://qjqmlgmvhhavbt:3a8f5fae4ffb8a6d94e53c4c77fd3a649a80c34129dd322c7f6e571175c865a6@ec2-34-197-141-7.compute-1.amazonaws.com:5432/df04rui3b64d3b",{table:"test"});
+const keyv = new Keyv("postgresql://postgres:arfw5369@localhost:5432/keyvtest",{table:"test"});
 keyv.on("error",e=>console.log("error:",e))
 async function main(){
     console.log("start");
